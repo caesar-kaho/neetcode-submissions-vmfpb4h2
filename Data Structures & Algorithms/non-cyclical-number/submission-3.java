7981189297f27c01cc40ebcbed18d1868f7a8df3
caesar-kaho <112843418+caesar-kaho@users.noreply.github.com>
2026-04-11 00:03:27 +0800

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> visited = new HashSet<>();
+        
+        return dfs(n, visited);
+    }
+
+    private boolean dfs(int n, HashSet<Integer> visited) {
+        if(n == 1) {
+            return true;
+        }
+
+        if(visited.contains(n)) {
+            return false;
+        }
+
+        visited.add(n);
+
+        int ans = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            ans += digit * digit;
+            n /= 10;
+        }
+
+        return dfs(ans, visited);
+    }
+}
